172ab5bc65f7cdc0e066c55c8dc772dc518c676c
mohand abdalbari <mohand.mostafa9811@gmail.com>
2020-04-25 12:46:05 +0200

    first edit in code for merge

--- js/homepage.js
+++ js/homepage.js
@@ -6,14 +6,15 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     scrollHorizontally: true,
     fadingEffect: true,
-    navigation: true,
-    navigationPosition: 'right',
+
+
 
 
     scrollingSpeed: 700,
     afterRender: function () {
-        let landingPageTitle = document.querySelector('h1:first-of-type ');
-        let landingPageImage = document.querySelector('img:first-of-type ');
+        let landingPageTitle = document.querySelector(' .HomeProductItem--ctn h2:first-of-type ');
+        console.log(landingPageTitle)
+        let landingPageImage = document.querySelector(' .HomeProductItem--ctn  img:first-of-type ');
         const tl = new TimelineMax({ delay: 0.5 })
         tl.fromTo(landingPageImage, 0.5, { y: '50', opacity: 0 }, { y: '0', opacity: 1, clearProps: "all" })
         tl.fromTo(landingPageTitle, 0.5, { y: '0', opacity: 0 }, { y: '-50%', opacity: 1, clearProps: "all" })
@@ -21,7 +22,7 @@ new fullpage('#fullpage', {
     },
     onLeave: function (origin, destination, direction) {
         const section = destination.item;
-        const title = section.querySelector('h1')
+        const title = section.querySelector('h2')
         const shoe = section.querySelector('img')
 
         const tl = new TimelineMax({ delay: 0.5 })
